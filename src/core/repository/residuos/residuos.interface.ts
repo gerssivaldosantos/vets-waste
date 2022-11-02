e683902a561ea597963cbab324063145522641cb
gerssivaldosantos <canalandows@gmail.com>
2022-11-02 14:18:23 -0300

    feat: add repository structure

---
+++ src/core/repository/residuos/residuos.interface.ts
@@ -0,0 +1,15 @@
+
+export type Residuo = {
+  id: string;
+  name: string;
+  description: string;
+  discard_method: string;
+}
+
+export interface ResiduosRepositoryInterface {
+  getResiduos():Promise<Residuo[]>
+  getResiduoById(id: string):Promise<Residuo>
+  createResiduo(data: Residuo):Promise<void>
+  updateResiduo(data: Residuo):Promise<void>
+  deleteResiduo(data: Residuo):Promise<void>
+}
