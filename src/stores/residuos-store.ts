d1b439ed6c9e260e9d62de53db4317f0ee3e9659
gerssivaldosantos <canalandows@gmail.com>
2022-11-02 16:51:14 -0300

    feat: create complete crud for residuo

--- src/stores/residuos-store.ts
+++ src/stores/residuos-store.ts
@@ -9,11 +9,19 @@ export const useResiduoStore = defineStore('residuo', {
     async create (residuo: Residuo):Promise<void> {
       this.residuos.push(residuo)
     },
-    async getById (id: string):Promise<Residuo | undefined> {
-      return this.residuos.find((residuo: Residuo) => residuo.id === id)
+    async getById (id: string):Promise<Residuo> {
+      const result = this.residuos.find((residuo: Residuo) => residuo.id === id)
+      if (result) return result
+      else throw new Error('Não foi possível buscar este resíduo')
     },
     async getResiduos ():Promise<Residuo[]> {
       return this.residuos
+    },
+    async deleteResiduo (id: string):Promise<void> {
+      this.residuos = this.residuos.filter((residuo:Residuo) => residuo.id !== id)
+    },
+    async updateResiduo (data: Residuo):Promise<void> {
+      this.residuos = this.residuos.map((residuo:Residuo) => residuo.id === data.id ? data : residuo)
     }
   }
 })
