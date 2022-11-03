885724a313b388154d09d6c1cf4e41a8a301e6c2
julia <juliaingrid.dev@gmail.com>
2022-11-02 21:53:21 -0300

    edita rows

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,53 +1,76 @@
 <template>
   <q-page class="page">
-    <q-item clickable v-ripple round class="items">
-      <q-item-section avatar>
-        <q-avatar rounded>
-          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-        </q-avatar>
-      </q-item-section>
-      <q-item-section>Image rounded avatar</q-item-section>
+    <q-item style="height: 150px;">
     </q-item>
-    <q-table
+    <h7>RESIDUOS</h7>
+   <q-table
       grid
       :rows="rows"
       row-key="name">
-      <template v-slot:item="{row}" class="">
-        <q-card>
-          {{ row.name }}
+      <template v-slot:item="{row}">
+        <q-card class="items">
+          <q-item >
+            <q-item-section avatar>
+            <q-avatar rounded>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              {{ row.name }}
+              {{row.description }}
+            </q-item-section>
+          </q-item>
         </q-card>
     </template>
     </q-table>
-    <q-btn round icon="add" class="bnt" />
+    <q-btn round icon="add" class="bnt" @click="addRow" />
    </q-page>
  </template>
 
 <script setup lang="ts">
+import { Residuo } from 'src/core/repository/residuos/residuos.repository.interface'
 
-const rows = [
+const rows : Residuo[] = [
   {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
+    id: '1',
+    name: 'Nome',
+    description: 'Descrição',
+    discard_method: 'dasd'
+  },
+  {
+    id: '2',
+    name: 'Nome 2',
+    description: 'Descrição 2',
+    discard_method: 'dasd'
   }
 ]
+
+const addRow = () => {
+  console.log('teste')
+}
+
 </script>
 
 <style lang="scss">
-*{
-  background: rgba(43, 192, 138, 0.05);
-}
-.items{
-  background: #FFFFFF;
-  max-width: 90%;
-  margin-left: 5%;
-  border-radius: 20px;
+.page{
+  background-color: rgba(43, 192, 139, 0.25);
+  padding-left: 2%;
+  padding-right: 2%;
 };
+
+.icon{
+  padding-right:30px ;
+}
+
+.items{
+  display: flex;
+  background: #FFFFFF;
+  border-radius: 1.5rem;
+  height: 5rem;
+  width: 100%;
+  margin-bottom: 5%;
+};
+
 .bnt{
   background: #135B46;
   position: absolute;
